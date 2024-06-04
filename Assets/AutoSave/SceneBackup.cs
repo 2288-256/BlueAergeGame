1dc729f39cc6bc4b14d890a4be08505fc322d964
2288-256 <74705442+2288-256@users.noreply.github.com>
2024-06-05 08:40:46 +0900

    add #22

---
+++ Assets/AutoSave/SceneBackup.cs
@@ -0,0 +1,18 @@
+#if DEBUG
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SceneBackup : UnityEditor.AssetModificationProcessor
+{
+	static string[] OnWillSaveAssets (string[] paths)
+	{
+		bool manualSave = AutoSave.IsManualSave;
+		if (manualSave) {
+			AutoSave.Backup ();
+		}
+
+		return paths;
+	}
+}
+#endif
